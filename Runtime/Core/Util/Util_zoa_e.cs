5735be00d0e9210c314b46ed83bf7da5a9643e33
ram16 <thomass.montaine@gmail.com>
2025-12-10 13:34:13 +0100

    ye

---
+++ Runtime/Core/Util/Util_zoa_e.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_EDITOR
+using TMPro;
+using UnityEditor;
+
+public static partial class Util_zoa_e
+{
+    [MenuItem("CONTEXT/" + nameof(TMP_InputField) + "/" + nameof(ToZSpaced))]
+    static void ToZSpaced(MenuCommand command)
+    {
+        TMP_InputField inputfield = (TMP_InputField)command.context;
+        inputfield.text = inputfield.text.ZSpaced();
+    }
+
+    [MenuItem("CONTEXT/" + nameof(TMP_InputField) + "/" + nameof(RemoveZSpaces))]
+    static void RemoveZSpaces(MenuCommand command)
+    {
+        TMP_InputField inputfield = (TMP_InputField)command.context;
+        inputfield.text = inputfield.text.UnZSpaced();
+    }
+}
+#endif
